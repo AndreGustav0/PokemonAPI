5ed143bbabd372c5b1e424b54cd641c6d60057e9
André Gustavo <andregustavodeoliveira49@gmail.com>
2024-09-11 16:59:46 -0300

    Adicionei CSS

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "/views"));
 
